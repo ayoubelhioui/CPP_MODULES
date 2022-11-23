dc4a17a47d710f9e777d1aeac015b43b52e75de6
Ayoub Elhioui <Elhioui001@gmail.com>
2022-11-23 09:32:32 +0100

    done

--- day03/ex02/main.cpp
+++ day03/ex02/main.cpp
@@ -6,20 +6,22 @@
 /*   By: ael-hiou <ael-hiou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 09:59:35 by ael-hiou          #+#    #+#             */
-/*   Updated: 2022/11/20 09:53:41 by ael-hiou         ###   ########.fr       */
+/*   Updated: 2022/11/20 18:27:08 by ael-hiou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "FragTrap.hpp"
-
+#include <vector>
+using namespace std;
 int main()
 {
 	FragTrap a("ayoub");
-    a.attack("unknown");
-    a.takeDamage(5);
-    FragTrap b = a;
-    b.attack("helloWorld");
-    b.highFivesGuys();
-    FragTrap j(a);
-    std::cout<<j.getName()<<std::endl;
+    FragTrap b(a);
+    // a.attack("unknown");
+    // a.takeDamage(5);
+    // FragTrap b = a;
+    // b.attack("helloWorld");
+    // b.highFivesGuys();
+    // FragTrap j(a);
+    // std::cout<<j.getName()<<std::endl;
 }
